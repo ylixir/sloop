250bc45361d3898ff04160cebe8c4020f04bfcab
Jon Allen <ylixir@gmail.com>
2016-02-24 04:57:20 -0600

    made a grid, made guys spawn on grid

--- src/game/main.js
+++ src/game/main.js
@@ -5,13 +5,15 @@ This work is free. You can redistribute it and/or modify it under the
 terms of the Do What The Fuck You Want To Public License, Version 2,
 as published by Sam Hocevar. See the COPYING file for more details.
  */
-chanceToDie = 0.5;
-liveCheckTime = 500; // in milliseconds?
 
 game.module('game.main').body(
 		function() {
 
+            chanceToDie = 0.5;
+            liveCheckTime = 1500; // in milliseconds?
+            
 			game.addAsset('panda.png');
+            
 			game.createClass('Swashbuckler',
 					{
 						speed : 100,
@@ -46,16 +48,15 @@ game.module('game.main').body(
 						remove : function() {
 							this.sprite.remove();
 						},
-						poop : function() {
-							var grap = new game.Graphics();
-							grap.fillColor = '#7A5230';
-							grap.drawCircle(this.sprite.position.x,
-									this.sprite.position.y, 10);
-							grap.addTo(game.scene.stage);
-						}
+				poop : function() {
+					var grap = new game.Graphics();
+					grap.fillColor = '#7A5230';
+					grap.drawCircle(this.sprite.position.x,
+							this.sprite.position.y, 10);
+					grap.addTo(game.scene.stage);
+                }
 					});
 			game.createClass('ScurvyDog', {
-
 				bearing : null,
 				init : function(x, y, vs, hs) {
 					this.sprite = new game.Sprite('panda.png')
@@ -81,13 +82,17 @@ game.module('game.main').body(
 						this.sprite.remove()
 						this.body.remove()
 						game.scene.removeObject(this)
-					} else
-						game.scene.addTimer(liveCheckTime, this.remove
-								.bind(this))
+					}
+                    else{
+                        this.poop()
+						game.scene.addTimer(liveCheckTime,
+                                            this.remove.bind(this))
+                    }
+                        
 				},
 				collide : function() {
-					this.bearing.x *= -1
-					this.bearing.y *= -1
+                    console.log("crash")
+                    this.bearing.multiply(-1)
 				},
 
 				update : function() {
@@ -108,88 +113,85 @@ game.module('game.main').body(
 				init : function() {
 					console.log('Press ctl-shift-i in chrome to see this');
 
+                    var cellSize = new game.Vector(game.width/18,game.height/18)
+                    
 					this.world = new game.World()
 					this.world.gravity.set(0, 0)
-
+					var wall = new game.Body()
+					wall.position.set(game.width / 2, game.height / 2)
+					wall.static = true;
+					wall.addShape(new game.Rectangle(
+                        cellSize.x*4,
+                        cellSize.y*4,
+                        cellSize.x*7,
+                        cellSize.y*7))
+					wall.addTo(this.world)
 					// ///////////////////////////////////////////////////
 					// I KNOW THE MATHEMATICIAN CAN FIGURE OUT A BETTER WAY TO
 					// DO THIS! DRAW THE GRID
+                    // sigh...fracking comp sci people.
+                    //      done with love,
+                    //      ylixir
+                    // p.s. if you ever need help figuring out the difference
+                    //      between a star and a grid, let me know.
 					// /////////////////////////////////////////////////////////
 					var grap = new game.Graphics();
+                    //the home base
+					grap.fillColor = '#666';
+                    grap.drawRect(
+                        wall.shape.x,
+                        wall.shape.y,
+                        wall.shape.width,
+                        wall.shape.height)
+                    //the grid
 					grap.fillColor = '#000000';
-					var xStart = game.width / 2;
-					var yStart = game.height / 2;
-					for (var i = 0; i < 10; i++) {
-						grap.drawCircle(xStart, yStart, 5);
-						xStart += 100;
-						yStart += 100;
-					}
-					xStart = game.width / 2;
-					yStart = game.height / 2;
-					for (var i = 0; i < 10; i++) {
-						grap.drawCircle(xStart, yStart, 5);
-						xStart -= 100;
-						yStart -= 100;
-					}
-					xStart = game.width / 2;
-					yStart = game.height / 2;
-					for (var i = 0; i < 10; i++) {
-						grap.drawCircle(xStart, yStart, 5);
-						xStart += 100;
-						yStart -= 100;
-					}
-					xStart = game.width / 2;
-					yStart = game.height / 2;
-					for (var i = 0; i < 10; i++) {
-						grap.drawCircle(xStart, yStart, 5);
-						xStart -= 100;
-						yStart += 100;
-					}
-					xStart = game.width / 2;
-					yStart = game.height / 2;
-					for (var i = 0; i < 5; i++) {
-						grap.drawCircle(xStart, yStart, 5);
-						yStart += 100;
-					}
-					xStart = game.width / 2;
-					yStart = game.height / 2;
-					for (var i = 0; i < 5; i++) {
-						grap.drawCircle(xStart, yStart, 5);
-						xStart += 100;
-					}
-					xStart = game.width / 2;
-					yStart = game.height / 2;
-					for (var i = 0; i < 5; i++) {
-						grap.drawCircle(xStart, yStart, 5);
-						yStart -= 100;
-					}
-					xStart = game.width / 2;
-					yStart = game.height / 2;
-					for (var i = 0; i < 5; i++) {
-						grap.drawCircle(xStart, yStart, 5);
-						xStart -= 100;
-					}
+                    for(var i=0;i<4;i++){ 
+                        for(var j=0;j<18;j++){
+                            grap.drawCircle(
+                                cellSize.x/2+cellSize.x*(7+i),
+                                cellSize.y/2+cellSize.y*j,
+                                5)
+                            grap.drawCircle(
+                                cellSize.x/2+cellSize.x*j,
+                                cellSize.y/2+cellSize.y*(7+i),
+                                5)
+                        }
+                    }
 					grap.addTo(game.scene.stage);
-					// ////////////////////////////////////////////////////////////////////////////////////////////
-					var walls = new game.Body()
-					walls.position.set(game.width / 2, game.height / 2)
-					walls.static = true;
-					walls.addShape(new game.Rectangle(100, game.height,
-							game.width, 0))
-					walls.addShape(new game.Rectangle(game.width, 100, 0,
-							game.height))
-					walls.addShape(new game.Rectangle(100, game.height, 0, 0))
-					walls.addShape(new game.Rectangle(game.width, 100, 0, 0))
-					walls.addTo(this.world)
+                    
 
 					this.player = new game.Swashbuckler()
 					this.addTimer(liveCheckTime, function() {
 						if (Math.random(0, 1) < 1 - chanceToDie) {
+                            //Math.random gives a value in the interval: [min,max)
+                            var position
+                            //get the axis
+                            if(Math.floor(Math.random(0,2)))
+                                position = new game.Vector(1,0)
+                            else
+                                position = new game.Vector(0,1)
+                            var bearing = new game.Vector(position.y,position.x)
+                            bearing.multiply(2*Math.floor(Math.random(0,2))-1)
+                            //get the lane
+                            position.multiply(Math.floor(Math.random(1,5)))
+                            
+                            //do math, make everything like it should be
+                            position.multiply(cellSize.x,cellSize.y)
+                            if(position.x) {
+                                position.y = -(bearing.y-1)/2*game.height
+                                position.x += cellSize.x*6
+                            }
+                            else {
+                                position.x = -(bearing.x-1)/2*game.width
+                                position.y += cellSize.y*6
+                            }
+                            bearing.multiply(100)
+                                
 							var pirate = new game.ScurvyDog(
-									game.width.random(), game.height.random(),
-									Math.random(-100, 100), Math.random(-100,
-											100));
-							// pirate.poop();
+                                position.x,
+                                position.y,
+                                bearing.x,
+                                bearing.y)
 						}
 
 					}, true);
